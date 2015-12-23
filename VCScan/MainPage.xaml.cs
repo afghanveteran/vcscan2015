e33c5a81d35212b1db59c969d968f65291389cc4
afghanveteran <predator1997>
2015-12-23 04:45:14 +0300

    First commit

---
+++ VCScan/MainPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Phone.UI.Input;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace VCScan
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        MediaCapture takePhotoManager;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+            else
+                Application.Current.Exit();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            InitCamera();
+        }
+
+        private static async Task<DeviceInformation> GetCameraID(Windows.Devices.Enumeration.Panel desired)
+        {
+            return (await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture))
+                .FirstOrDefault(x => x.EnclosureLocation != null && x.EnclosureLocation.Panel == desired);
+        }
+
+        async private void InitCamera()
+        {
+            var cameraID = await GetCameraID(Windows.Devices.Enumeration.Panel.Back);
+            takePhotoManager = new MediaCapture();
+            await takePhotoManager.InitializeAsync(new MediaCaptureInitializationSettings
+            {
+                StreamingCaptureMode = StreamingCaptureMode.Video,
+                PhotoCaptureSource = PhotoCaptureSource.Photo,
+                AudioDeviceId = string.Empty,
+                VideoDeviceId = cameraID.Id
+            });
+
+            photoPreview.Source = takePhotoManager;
+         
+            await takePhotoManager.StartPreviewAsync();
+        }
+
+        private async void btnPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            btnPhoto.IsEnabled = false;
+
+            ImageEncodingProperties imgFormat = ImageEncodingProperties.CreatePng();
+            imgFormat.Width = 2572;
+            imgFormat.Height = 1228;
+
+            StorageFile file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                    "photo.png", CreationCollisionOption.ReplaceExisting);
+
+            await takePhotoManager.CapturePhotoToStorageFileAsync(imgFormat, file);
+
+            BitmapImage bmpImage = new BitmapImage(new Uri(file.Path));
+            myImage.Source = bmpImage;
+
+
+            btnScan.Visibility = Visibility.Visible;
+            btnDelete.Visibility = Visibility.Visible;
+
+            myImage.Visibility = Visibility.Visible;
+        }
+
+        private async void photoPreview_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            takePhotoManager.VideoDeviceController.FocusControl.Configure(new Windows.Media.Devices.FocusSettings { Mode = Windows.Media.Devices.FocusMode.Auto });
+            await takePhotoManager.VideoDeviceController.FocusControl.FocusAsync();
+        }
+
+        private async void btnScan_Click(object sender, RoutedEventArgs e)
+        {
+            await takePhotoManager.StopPreviewAsync();
+            Frame.Navigate(typeof(ScanPage));
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            myImage.Source = null;
+            myImage.Visibility = Visibility.Collapsed;
+
+            btnDelete.Visibility = Visibility.Collapsed;
+            btnScan.Visibility = Visibility.Collapsed;
+
+            btnPhoto.IsEnabled = true;
+        }
+    }
+}
---
+++ VCScan/ScanPage.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using WindowsPreview.Media.Ocr;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace VCScan
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ScanPage : Page
+    {
+        OcrEngine ocrEngine;
+        UInt32 width;
+        UInt32 height;
+
+        string recognizedText;
+
+        public ScanPage()
+        {
+            this.InitializeComponent();
+
+            ocrEngine = new OcrEngine(OcrLanguage.English);
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var file = await ApplicationData.Current.LocalFolder.GetFileAsync("photo.png");
+            using (var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+            {
+                
+                var decoder = await BitmapDecoder.CreateAsync(stream);
+
+               
+                width = decoder.PixelWidth;
+                height = decoder.PixelHeight;
+
+                
+                var pixels = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Straight,
+                    new BitmapTransform(),
+                    ExifOrientationMode.RespectExifOrientation,
+                    ColorManagementMode.ColorManageToSRgb);
+
+               
+                OcrResult result = await ocrEngine.RecognizeAsync(height, width, pixels.DetachPixelData());
+
+                recognizedText = "";
+              
+                if (result.Lines != null)
+                {
+                    foreach (var line in result.Lines)
+                    {
+                        foreach (var word in line.Words)
+                        {
+                            recognizedText += word.Text + " ";
+                            OcrText.Text += word.Text + " ";
+                        }
+                        OcrText.Text += Environment.NewLine;
+                    }
+                }
+                txtEmail.Text += " " + scanEmails(recognizedText);
+                txtPhone.Text += " " + scanPhones(recognizedText);
+            }
+        }
+
+        private string scanEmails(string text)
+        {
+            const string MatchEmailPattern =
+               @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+             + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+             + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+             + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+            Regex rx = new Regex(MatchEmailPattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+            MatchCollection emailMatches = rx.Matches(text);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Match emailMatch in emailMatches)
+                sb.AppendLine(emailMatch.Value + " ");
+            return sb.ToString();
+        }
+
+        private string scanPhones(string text)
+        {
+            const string MatchPhonePattern = @"((8|\+7)-?)?\(?\d{3}\)?-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}-?\d{1}";
+            Regex rx = new Regex(MatchPhonePattern);
+
+            MatchCollection phonesMatches = rx.Matches(text);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Match phoneMatch in phonesMatches)
+                sb.AppendLine(phoneMatch.Value + " ");
+            return sb.ToString();
+        }
+    }
+}
